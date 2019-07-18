cc378e43ad8a867ab274009f0315c2b936915bb6
ryansims265 <ryansims265@gmail.com>
2019-07-18 19:18:45 -0400

    Added comments in the JS file to explain each area

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//First setup the firebase database 
+//First console log just to test that the JS file is linked correctly 
 console.log("Waiting for Submission");
 
-
+//Set up a timer to display the current time to the user and update every second 
 setInterval(function(){
   var currentTime = moment().format("HH:MM A");
   $("#currentTime").html("The Current Time is: " + currentTime);
 }, 1000);
 
-// var today = new Date();
-// var currenttime = today.getHours() + ":" + today.getMinutes();
-// $("#timer").html("Time until next Train: " + currenttime);
-
+//Setup the firebase database 
 var firebaseConfig = {
   apiKey: "AIzaSyA2dI2zWOEsFCpHiCDs5T4200ax98VJRb8",
   authDomain: "anotherone-631a5.firebaseapp.com",
@@ -22,16 +19,13 @@ var firebaseConfig = {
 };
 //Then intialize the firebase database that we just setup
 firebase.initializeApp(firebaseConfig);
-
 //Then set up a variable that calls the database
 var database = firebase.database();
-
-<<<<<<< HEAD
-  //Create a call to the submit button that waits for the click 
+//Listen for a click on the submit button 
   $("#submit-button").on("click", function(event) {
     event.preventDefault();
     console.log("Submitted");
-//Create variables for each of the user submission areas 
+//Create variables for each of the user submission areas and log each one 
     var name = $("#input-Name").val().trim();
     var destination = $("#input-Dest").val().trim();
     var start = $("#input-Start").val().trim();
@@ -40,39 +34,6 @@ var database = firebase.database();
     console.log(destination);
     console.log(start);
     console.log(freq);
-    var newtime = Data.parse(start);
-    console.log(newtime);
-
-    //Then push the user data values to the database
-    database.ref().push({
-      name: name,
-      destination: destination,
-      start: start,
-      freq: freq,
-      currenttime: currenttime,
-    });
-//Clear the input boxes after submission 
-=======
-//Create a call to the submit button that waits for the click 
-$("#submit-button").on("click", function (event) {
-  event.preventDefault();
-  console.log("Submitted");
-  //Create variables for each of the user submission areas 
-  var number = $("#input-Number").val().trim();
-  var line = $("#input-Line").val().trim();
-  var destination = $("#input-Dest").val().trim();
-  var start = $("#input-Start").val().trim();
-  var freq = $("#input-Freq").val().trim();
-  var platform = $("#input-Platform").val().trim();
-  console.log(number);
-  console.log(destination);
-  console.log(start);
-  console.log(freq);
-  console.log(line);
-
-  var nextarrival = "test";
-  var timetoarrive = "test";
->>>>>>> e8603b919ce42e815ac749fd9c55f61512176d7a
 
   //Then push the user data values to the database
   database.ref().push({
@@ -85,6 +46,7 @@ $("#submit-button").on("click", function (event) {
     timetoarrive: timetoarrive,
     platform: platform,
   });
+
   //Clear the input boxes after submission 
 
   $("#input-Number").val("");
@@ -93,30 +55,9 @@ $("#submit-button").on("click", function (event) {
   $("#input-Freq").val("");
   $("#input-Line").val("");
   $("#input-Platform").val("");
-
-
 });
 
-
 //Next set up a snapshot so that it will update the DOM if the data changes at any time 
-<<<<<<< HEAD
-    database.ref().on("child_added", function(snapshot) {
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().destination);
-      console.log(snapshot.val().start);
-      console.log(snapshot.val().freq);
-      console.log(snapshot.val().currenttime);
-      // Change the HTML
-$("#table").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().start + "</td><td>" + snapshot.val().freq) + "</td></tr>";
-
-//Setup error handling 
-
-    }, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-
-   
-=======
 database.ref().on("child_added", function (snapshot) {
   console.log(snapshot.val().number);
   console.log(snapshot.val().line);
@@ -127,11 +68,10 @@ database.ref().on("child_added", function (snapshot) {
   console.log(snapshot.val().timetoarrive);
   console.log(snapshot.val().platform);
 
-  // Change the HTML
+  // Change the HTML to reflect the changes in the snapshot
   $("#table").append("<tr><td>" + snapshot.val().number + "</td><td>" + snapshot.val().line + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().start + "</td><td>" + snapshot.val().freq + "</td><td>" + snapshot.val().nextarrival + "</td><td>" + snapshot.val().timetoarrive + "</td><td>" + snapshot.val().platform + "</td></tr>");
   //Setup error handling 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
->>>>>>> e8603b919ce42e815ac749fd9c55f61512176d7a
 
