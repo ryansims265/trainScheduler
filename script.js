0d5e6f8863c2659d13d75d15d7e99d3618499827
ryansims265 <ryansims265@gmail.com>
2019-07-18 20:31:36 -0400

    Figured out timeuntil function for upcoming trains

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Waiting for Submission");
 //Set up a timer to display the current time to the user and update every second 
 setInterval(function(){
   var currentTime = moment().format("HH:MM A");
-  $("#currentTime").html("The Current Time is: " + currentTime);
+  $("#currentTime").html("Current Time: " + currentTime);
 }, 1000);
 
 //Setup the firebase database 
@@ -26,28 +26,60 @@ var database = firebase.database();
     event.preventDefault();
     console.log("Submitted");
 //Create variables for each of the user submission areas and log each one 
-    var name = $("#input-Name").val().trim();
-    var destination = $("#input-Dest").val().trim();
-    var start = $("#input-Start").val().trim();
-    var freq = $("#input-Freq").val().trim();
-    console.log(name);
+    var number = $("#input-Number").val();
+    var line = $("#input-Line").val();
+    var destination = $("#input-Dest").val();
+    var start = $("#input-Start").val();
+    var freq = $("#input-Freq").val();
+    var platform = $("#input-Platform").val();
+    console.log(number);
+    console.log(line);
     console.log(destination);
     console.log(start);
     console.log(freq);
+    console.log(platform);
 
+     function nextArrival() {
+	    // Departure time
+	    var departure = moment(start, "hh:mm").subtract(1, 'years');
+	    //Difference between the departure and the current time 
+	    var timeDiff = moment().diff(moment(departure), "minutes");
+	    //Now we must find the remainder between the time difference and the frequency of the train 
+      var remainder = timeDiff % freq;
+	    //This then is subtracted from the frequency which gives us the number of minutes until the next train 
+      var untilNext = freq - remainder;
+      console.log("Time until next train: " + untilNext);
+	    // //The next train is then formated 
+	    // var nextarrival = moment().add(untilNext, 'minutes');
+      // nextarrival = moment(nextarrival).format('h:mm A');
+      // console.log(nextarrival);
+      database.ref().push({
+        number: number,
+        line: line,
+        destination: destination,
+        start: start,
+        freq: freq,
+        untilNext: untilNext,
+        platform: platform,
+      });
+  }
+  
+nextArrival();
+
+ 
+  
   //Then push the user data values to the database
-  database.ref().push({
-    number: number,
-    line: line,
-    destination: destination,
-    start: start,
-    freq: freq,
-    nextarrival: nextarrival,
-    timetoarrive: timetoarrive,
-    platform: platform,
-  });
+  // database.ref().push({
+  //   number: number,
+  //   line: line,
+  //   destination: destination,
+  //   start: start,
+  //   freq: freq,
+  //   untilNext: untilNext,
+  //   platform: platform,
+  // });
 
-  //Clear the input boxes after submission 
+  //Clear the input boxes after submission {nextarrival: nextarrival,} {timetoarrive: timetoarrive,} 
 
   $("#input-Number").val("");
   $("#input-Dest").val("");
@@ -59,19 +91,18 @@ var database = firebase.database();
 
 //Next set up a snapshot so that it will update the DOM if the data changes at any time 
 database.ref().on("child_added", function (snapshot) {
-  console.log(snapshot.val().number);
-  console.log(snapshot.val().line);
-  console.log(snapshot.val().destination);
-  console.log(snapshot.val().start);
-  console.log(snapshot.val().freq);
-  console.log(snapshot.val().nextarrival);
-  console.log(snapshot.val().timetoarrive);
-  console.log(snapshot.val().platform);
+  console.log("Snapshot: " + snapshot.val().number);
+  console.log("Snapshot: " + snapshot.val().line);
+  console.log("Snapshot: " + snapshot.val().destination);
+  console.log("Snapshot: " + snapshot.val().start);
+  console.log("Snapshot: " + snapshot.val().freq);
+  console.log("Snapshot: " + snapshot.val().untilNext);
+  // console.log(snapshot.val().timetoarrive);
+  console.log("Snapshot: " + snapshot.val().platform);
 
   // Change the HTML to reflect the changes in the snapshot
-  $("#table").append("<tr><td>" + snapshot.val().number + "</td><td>" + snapshot.val().line + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().start + "</td><td>" + snapshot.val().freq + "</td><td>" + snapshot.val().nextarrival + "</td><td>" + snapshot.val().timetoarrive + "</td><td>" + snapshot.val().platform + "</td></tr>");
+  $("#table").append("<tr><td>" + snapshot.val().number + "</td><td>" + snapshot.val().line + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().start + "</td><td>" + snapshot.val().freq + "</td><td>" + + snapshot.val().untilNext + "</td><td>" + snapshot.val().platform + "</td></tr>");
   //Setup error handling 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
-
